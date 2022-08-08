3bfc699e533542ad8b66825856924b0717ffe830
Valery Zubkov <bouddha@mail.ru>
2022-08-08 16:10:07 +0400

    add project array_while

---
+++ project_010/Program.cs
@@ -0,0 +1,12 @@
+﻿int [] array = {154, 122, 543, 844, 65, 96, 547, 668};
+int n = array.Length;
+int find = 547;
+int index = 0;
+while(index < n)
+{
+    if(array[index] == find) 
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
